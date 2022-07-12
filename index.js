4ad90637dd1ffbfb2b754e1b259cf89be7b49a8f
jago-gif <halcon1411@gmail.com>
2022-07-12 04:42:13 -0400

    paso a produccion

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json({ extended: true }));
 app.use('/api/productos', require('./routes/producto'));
 
 
-
-app.listen(4000, () => {    
-    console.log("Server is running on port 4000")
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {    
+    console.log("Server is running on port " + PORT);
  });
